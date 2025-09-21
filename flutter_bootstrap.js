5fb352db83b329a889f2ceb382b1e7bf16a8362a
Shaxboz <shaxboz08011990@gmail.com>
2025-09-22 01:17:45 +0500

    1.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "176771313"
+    serviceWorkerVersion: "3216638411"
   }
 });
